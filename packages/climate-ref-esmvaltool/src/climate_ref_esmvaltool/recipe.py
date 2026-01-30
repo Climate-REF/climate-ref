d7ed8552e89ad9d7b6c4096504ac2ee7b7b2778f
Bouwe Andela <b.andela@esciencecenter.nl>
2026-01-30 16:18:04 +0100

    Update ESMValTool to v2.13.0

--- packages/climate-ref-esmvaltool/src/climate_ref_esmvaltool/recipe.py
+++ packages/climate-ref-esmvaltool/src/climate_ref_esmvaltool/recipe.py
@@ -144,8 +144,9 @@ def dataframe_to_recipe(
     return variables
 
 
-_ESMVALTOOL_COMMIT = "2c438d0e0cc8904790294c72450eb7f06552c52a"
-_ESMVALTOOL_VERSION = f"2.13.0.dev148+g{_ESMVALTOOL_COMMIT[:9]}"
+_ESMVALTOOL_COMMIT = "fa3e10ed09fc34d354f179a6a71aacd032d6ea9e"
+# _ESMVALTOOL_VERSION = f"2.13.0.dev148+g{_ESMVALTOOL_COMMIT[:9]}"
+_ESMVALTOOL_VERSION = "2.13.0"
 
 _RECIPES = pooch.create(
     path=pooch.os_cache("climate_ref_esmvaltool"),

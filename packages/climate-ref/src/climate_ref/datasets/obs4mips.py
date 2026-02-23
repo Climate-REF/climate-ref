b49ae0f50b3302cd7c4646470294beaa88fe2169
Jared Lewis <jared@jared.kiwi.nz>
2026-02-23 11:28:53 +1100

    chore: enforce known converted file

--- packages/climate-ref/src/climate_ref/datasets/obs4mips.py
+++ packages/climate-ref/src/climate_ref/datasets/obs4mips.py
@@ -190,12 +190,14 @@ class Obs4MIPsDatasetAdapter(DatasetAdapter):
             self.version_metadata,
         ]
         datasets["instance_id"] = datasets.apply(
-            lambda row: "obs4MIPs."
-            + ".".join(
-                [
-                    row[item].replace(" ", "") if item == "nominal_resolution" else row[item]
-                    for item in drs_items
-                ]
+            lambda row: (
+                "obs4MIPs."
+                + ".".join(
+                    [
+                        row[item].replace(" ", "") if item == "nominal_resolution" else row[item]
+                        for item in drs_items
+                    ]
+                )
             ),
             axis=1,
         )

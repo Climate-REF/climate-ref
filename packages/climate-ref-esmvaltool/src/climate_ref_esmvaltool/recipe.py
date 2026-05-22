c86d5b85cf42d6108f9e070643f8191c73411ebb
Manuel Schlund <manuel.schlund@dlr.de>
2026-05-22 12:32:39 +0200

    Made TCR diagnostic work with CMIP7 data

--- packages/climate-ref-esmvaltool/src/climate_ref_esmvaltool/recipe.py
+++ packages/climate-ref-esmvaltool/src/climate_ref_esmvaltool/recipe.py
@@ -296,7 +296,7 @@ def get_child_and_parent_dataset(
     return [child_dataset, parent_dataset]
 
 
-_ESMVALTOOL_COMMIT = "dead90ca848d3892ed90785b3940909d58d27780"
+_ESMVALTOOL_COMMIT = "4168f12dcb3fdabfd4eedf21217632ef16aa448a"
 _ESMVALTOOL_VERSION = f"2.14.0.dev65+g{_ESMVALTOOL_COMMIT[:9]}"
 _ESMVALTOOL_URL = f"git+https://github.com/ESMValGroup/ESMValTool.git@{_ESMVALTOOL_COMMIT}"
 
